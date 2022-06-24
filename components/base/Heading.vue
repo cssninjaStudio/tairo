325806ec31712de27d4758221d1109763c25d058
Sacha STAFYNIAK <sacha@digisquad.io>
2022-06-24 19:28:52 +0200

    fix(base-heading): use component instead of v-ifs

--- components/base/Heading.vue
+++ components/base/Heading.vue
@@ -48,8 +48,8 @@ const props = withDefaults(defineProps<HeadingProps>(), {
 </script>
 
 <template>
-  <h1
-    v-if="props.tag === 'h1'"
+  <component
+    :is="props.tag"
     class="font-main"
     :class="[
       props.size === 'xs' && 'text-xs',
@@ -80,104 +80,5 @@ const props = withDefaults(defineProps<HeadingProps>(), {
     ]"
   >
     <slot></slot>
-  </h1>
-  <h2
-    v-else-if="props.tag === 'h2'"
-    class="font-main"
-    :class="[
-      props.size === 'xs' && 'text-xs',
-      props.size === 'sm' && 'text-sm',
-      props.size === 'md' && 'text-base',
-      props.size === 'lg' && 'text-lg',
-      props.size === 'xl' && 'text-xl',
-      props.size === '2xl' && 'text-2xl',
-      props.size === '3xl' && 'text-3xl',
-      props.size === '4xl' && 'text-4xl',
-      props.size === '5xl' && 'text-5xl',
-      props.size === '6xl' && 'text-6xl',
-      props.size === '7xl' && 'text-7xl',
-      props.size === '8xl' && 'text-8xl',
-      props.size === '9xl' && 'text-9xl',
-      props.weight === 'light' && 'font-light',
-      props.weight === 'normal' && 'font-normal',
-      props.weight === 'medium' && 'font-medium',
-      props.weight === 'semibold' && 'font-semibold',
-      props.weight === 'bold' && 'font-bold',
-      props.weight === 'extrabold' && 'font-extrabold',
-      props.lead === 'none' && 'leading-none',
-      props.lead === 'normal' && 'leading-normal',
-      props.lead === 'tight' && 'leading-tight',
-      props.lead === 'snug' && 'leading-snug',
-      props.lead === 'normal' && 'leading-normal',
-      props.lead === 'loose' && 'leading-loose',
-    ]"
-  >
-    <slot></slot>
-  </h2>
-  <h3
-    v-else-if="props.tag === 'h3'"
-    class="font-main"
-    :class="[
-      props.size === 'xs' && 'text-xs',
-      props.size === 'sm' && 'text-sm',
-      props.size === 'md' && 'text-base',
-      props.size === 'lg' && 'text-lg',
-      props.size === 'xl' && 'text-xl',
-      props.size === '2xl' && 'text-2xl',
-      props.size === '3xl' && 'text-3xl',
-      props.size === '4xl' && 'text-4xl',
-      props.size === '5xl' && 'text-5xl',
-      props.size === '6xl' && 'text-6xl',
-      props.size === '7xl' && 'text-7xl',
-      props.size === '8xl' && 'text-8xl',
-      props.size === '9xl' && 'text-9xl',
-      props.weight === 'light' && 'font-light',
-      props.weight === 'normal' && 'font-normal',
-      props.weight === 'medium' && 'font-medium',
-      props.weight === 'semibold' && 'font-semibold',
-      props.weight === 'bold' && 'font-bold',
-      props.weight === 'extrabold' && 'font-extrabold',
-      props.lead === 'none' && 'leading-none',
-      props.lead === 'normal' && 'leading-normal',
-      props.lead === 'tight' && 'leading-tight',
-      props.lead === 'snug' && 'leading-snug',
-      props.lead === 'normal' && 'leading-normal',
-      props.lead === 'loose' && 'leading-loose',
-    ]"
-  >
-    <slot></slot>
-  </h3>
-  <h4
-    v-else-if="props.tag === 'h4'"
-    class="font-main"
-    :class="[
-      props.size === 'xs' && 'text-xs',
-      props.size === 'sm' && 'text-sm',
-      props.size === 'md' && 'text-base',
-      props.size === 'lg' && 'text-lg',
-      props.size === 'xl' && 'text-xl',
-      props.size === '2xl' && 'text-2xl',
-      props.size === '3xl' && 'text-3xl',
-      props.size === '4xl' && 'text-4xl',
-      props.size === '5xl' && 'text-5xl',
-      props.size === '6xl' && 'text-6xl',
-      props.size === '7xl' && 'text-7xl',
-      props.size === '8xl' && 'text-8xl',
-      props.size === '9xl' && 'text-9xl',
-      props.weight === 'light' && 'font-light',
-      props.weight === 'normal' && 'font-normal',
-      props.weight === 'medium' && 'font-medium',
-      props.weight === 'semibold' && 'font-semibold',
-      props.weight === 'bold' && 'font-bold',
-      props.weight === 'extrabold' && 'font-extrabold',
-      props.lead === 'none' && 'leading-none',
-      props.lead === 'normal' && 'leading-normal',
-      props.lead === 'tight' && 'leading-tight',
-      props.lead === 'snug' && 'leading-snug',
-      props.lead === 'normal' && 'leading-normal',
-      props.lead === 'loose' && 'leading-loose',
-    ]"
-  >
-    <slot></slot>
-  </h4>
+  </component>
 </template>
