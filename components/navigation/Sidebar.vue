d4acb7c1af4d1bd463bbee462b6aa1e425e8f85c
Sacha STAFYNIAK <sacha@digisquad.io>
2022-06-26 18:46:31 +0200

    feat(sidebar): add subnav-header slot

--- components/navigation/Sidebar.vue
+++ components/navigation/Sidebar.vue
@@ -56,11 +56,21 @@ const endSidebars = sidebars.filter((sidebar) => sidebar.position === 'end')
       >
         <!-- Header -->
         <div class="flex items-center h-16 w-full px-6">
-          <h2
-            class="font-main text-lg font-semibold text-gray-700 dark:text-white capitalize"
+          <slot
+            name="subnav-header"
+            v-bind="{
+              activeSidebar,
+              sidebars,
+              isSidebarOpened,
+              toggleActiveSidebar,
+            }"
           >
-            {{ activeSidebar.name }}
-          </h2>
+            <h2
+              class="font-main text-lg font-semibold text-gray-700 dark:text-white capitalize"
+            >
+              {{ activeSidebar.name }}
+            </h2>
+          </slot>
           <button
             class="flex xl:hidden items-center justify-center h-10 w-10 rounded-full ml-auto text-slate-400 hover:text-slate-600 hover:bg-slate-100 transition-colors duration-300"
             @click="isSidebarOpened = false"
