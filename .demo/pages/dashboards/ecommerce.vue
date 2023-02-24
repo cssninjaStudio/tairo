d607441a3be36e1c84c52d2601201863f3d69461
Driss Chelouati <driss.chelouati@gmail.com>
2023-02-24 12:30:43 +0100

    fix: order chart loading

--- .demo/pages/dashboards/ecommerce.vue
+++ .demo/pages/dashboards/ecommerce.vue
@@ -599,19 +599,113 @@ function useBarOrders() {
       series.value.push({
         name: 'Orders',
         data: [
-          // ...
+          {
+            x: 'Jan',
+            y: 322,
+          },
+          {
+            x: 'Feb',
+            y: 459,
+          },
+          {
+            x: 'Mar',
+            y: 212,
+          },
+          {
+            x: 'Apr',
+            y: 345,
+          },
+          {
+            x: 'May',
+            y: 111,
+          },
+          {
+            x: 'Jun',
+            y: 189,
+          },
+          {
+            x: 'Jul',
+            y: 498,
+          },
+          {
+            x: 'Aug',
+            y: 612,
+          },
+          {
+            x: 'Sep',
+            y: 451,
+          },
+          {
+            x: 'Oct',
+            y: 248,
+          },
+          {
+            x: 'Nov',
+            y: 306,
+          },
+          {
+            x: 'Dec',
+            y: 366,
+          },
         ],
       })
-    }, 5000)
+    }, 1500)
 
     timeout2 = setTimeout(() => {
       series.value.push({
         name: 'Abandonned',
         data: [
-          // ...
+          {
+            x: 'Jan',
+            y: 25,
+          },
+          {
+            x: 'Feb',
+            y: 49,
+          },
+          {
+            x: 'Mar',
+            y: 36,
+          },
+          {
+            x: 'Apr',
+            y: 84,
+          },
+          {
+            x: 'May',
+            y: 64,
+          },
+          {
+            x: 'Jun',
+            y: 131,
+          },
+          {
+            x: 'Jul',
+            y: 48,
+          },
+          {
+            x: 'Aug',
+            y: 144,
+          },
+          {
+            x: 'Sep',
+            y: 96,
+          },
+          {
+            x: 'Oct',
+            y: 11,
+          },
+          {
+            x: 'Nov',
+            y: 31,
+          },
+          {
+            x: 'Dec',
+            y: 8,
+          },
         ],
       })
-    }, 7500)
+    }, 2500)
   })
   onBeforeUnmount(() => {
     clearTimeout(timeout)
