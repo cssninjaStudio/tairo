9b09ab36bb1354acc2d9c66913560556811439b1
Driss Chelouati <driss.chelouati@gmail.com>
2023-03-23 00:40:35 +0000

    feat: profile settings

--- .demo/pages/layouts/profile-settings.vue
+++ .demo/pages/layouts/profile-settings.vue
@@ -71,7 +71,9 @@ const { data, pending, error, refresh } = await useFetch('/api/profile/')
                 >{{ data?.personalInfo.relations }}+ relations</BaseText
               >
             </div>
-            <div class="h-8 flex items-center gap-1 px-4 text-muted-400">
+            <div
+              class="h-8 hidden sm:flex items-center gap-1 px-4 text-muted-400"
+            >
               <Icon name="ph:check-circle-duotone" class="w-5 h-5" />
               <BaseText size="sm"
                 >{{ data?.personalInfo.projects }} projects</BaseText
@@ -92,10 +94,12 @@ const { data, pending, error, refresh } = await useFetch('/api/profile/')
         </div>
       </div>
 
-      <div class="grid sm:grid-cols-5 gap-4 mt-6">
+      <div
+        class="w-full max-w-4xl mx-auto grid grid-cols-2 sm:grid-cols-4 ltablet:grid-cols-5 lg:grid-cols-5 gap-4 mt-6"
+      >
         <BaseCard
           shape="curved"
-          shadow="hover"
+          elevated-hover
           class="group border-2 hover:!border-primary-500"
         >
           <NuxtLink to="#" class="block p-6">
@@ -104,7 +108,12 @@ const { data, pending, error, refresh } = await useFetch('/api/profile/')
                 name="ph:buildings-duotone"
                 class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
               />
-              <BaseHeading tag="h3" size="sm" weight="medium" class="mt-2">
+              <BaseHeading
+                tag="h3"
+                size="sm"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
                 Company
               </BaseHeading>
               <BaseText size="xs" class="text-muted-400">
@@ -115,7 +124,32 @@ const { data, pending, error, refresh } = await useFetch('/api/profile/')
         </BaseCard>
         <BaseCard
           shape="curved"
-          shadow="hover"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:users-four-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="sm"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Team
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                Manage team
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
           class="group border-2 hover:!border-primary-500"
         >
           <NuxtLink to="#" class="block p-6">
@@ -124,11 +158,191 @@ const { data, pending, error, refresh } = await useFetch('/api/profile/')
                 name="ph:briefcase-duotone"
                 class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
               />
-              <BaseHeading tag="h3" size="sm" weight="medium" class="mt-2">
+              <BaseHeading
+                tag="h3"
+                size="sm"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
                 Projects
               </BaseHeading>
               <BaseText size="xs" class="text-muted-400">
                 Project settings
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:lock-open-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="xs"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Permissions
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                Manage permissions
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:note-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="xs"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Documents
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                Data privacy
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:upload-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="xs"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Upload
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                Upload settings
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:credit-card-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="xs"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Billing
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                Billing and plans
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:chat-circle-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="xs"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Messaging
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                Chat settings
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:shield-check-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="xs"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Security
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                Security settings
+              </BaseText>
+            </div>
+          </NuxtLink>
+        </BaseCard>
+        <BaseCard
+          shape="curved"
+          elevated-hover
+          class="group border-2 hover:!border-primary-500"
+        >
+          <NuxtLink to="#" class="block p-6">
+            <div class="text-center">
+              <Icon
+                name="ph:gear-six-duotone"
+                class="group-hover:text-primary-500 group-hover:rotate-6 w-8 h-8 text-muted-400 transition-all duration-300"
+              />
+              <BaseHeading
+                tag="h3"
+                size="xs"
+                weight="semibold"
+                class="uppercase !text-[0.65rem] mt-2"
+              >
+                Preferences
+              </BaseHeading>
+              <BaseText size="xs" class="text-muted-400">
+                General settings
               </BaseText>
             </div>
           </NuxtLink>
