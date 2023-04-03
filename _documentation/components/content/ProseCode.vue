e872481cb0f6f3395998996ca056b1aad93ed1d5
Sacha STAFYNIAK <sacha@digisquad.io>
2023-04-03 19:24:58 +0200

    docs: create layers index content

--- _documentation/components/content/ProseCode.vue
+++ _documentation/components/content/ProseCode.vue
@@ -23,15 +23,29 @@ export default defineComponent({
       default: null,
     },
   },
+  setup: (props) => {
+    const markdown = computed(() => {
+      return `\`\`\`${props.language}\n${props.code}\`\`\``
+    })
+
+    return {
+      markdown,
+    }
+  },
 })
 </script>
 
 <template>
   <div>
-    testeae
-    <div>
-      <slot />
-    </div>
+    <AddonMarkdownRemark
+      :source="markdown"
+      fullwidth
+      class="doc-markdown"
+      :theme="{
+        light: 'cssninja-light-theme',
+        dark: 'cssninja-dark-theme',
+      }"
+    />
   </div>
 </template>
 
