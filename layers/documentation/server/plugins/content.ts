921dd781aa2d641a33e2d5bba250c5541fe54859
Sacha STAFYNIAK <sacha@digisquad.io>
2024-04-15 12:57:56 +0200

    chore: update nuxt content plugin

--- layers/documentation/server/plugins/content.ts
+++ layers/documentation/server/plugins/content.ts
@@ -1,15 +1,10 @@
 import { version } from '../../../../package.json'
 
-const markdownRe = /\.md$/g
-const placeholderRe = /__TAIRO_LAYERS_VERSION__/g
-
 export default defineNitroPlugin((nitroApp) => {
   // @ts-ignore
   nitroApp.hooks.hook('content:file:beforeParse', (file) => {
-    if (!markdownRe.test(file.path)) {
-      return
+    if (file._id.endsWith('.md')) {
+      file.body = file.body.replaceAll('__TAIRO_LAYERS_VERSION__', `v${version}`)
     }
-
-    file.body = file.body.replace(placeholderRe, `v${version}`)
   })
 })
