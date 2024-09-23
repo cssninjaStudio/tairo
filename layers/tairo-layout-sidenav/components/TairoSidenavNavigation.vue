3dddce06e9408c6a218eee3879474df459da3905
Driss Chelouati <driss.chelouati@gmail.com>
2024-09-23 12:41:51 +0200

    fix: click event not being triggered in sidenav layout

--- layers/tairo-layout-sidenav/components/TairoSidenavNavigation.vue
+++ layers/tairo-layout-sidenav/components/TairoSidenavNavigation.vue
@@ -231,6 +231,7 @@ const isSearchOpen = useState('search-open', () => false)
                     v-else
                     type="button"
                     class="text-muted-500 hover:text-muted-900 hover:before:bg-muted-50 focus-visible:before:ring-primary-500 dark:text-muted-400 dark:hover:before:bg-muted-800/50 dark:focus-visible:before:ring-primary-400 group relative flex w-full cursor-pointer items-center gap-1.5 rounded-md px-2.5 py-1.5 text-sm font-medium before:absolute before:inset-px before:rounded-md focus:outline-none focus-visible:outline-none focus-visible:before:ring-2 focus-visible:before:ring-inset disabled:cursor-not-allowed disabled:opacity-75 dark:hover:text-white dark:focus-visible:outline-none"
+                    @click="item.click"
                   >
                     <Icon
                       :name="item.icon?.name"
