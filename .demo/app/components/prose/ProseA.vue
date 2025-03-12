36bf637977f2724b6978f1e19c2a25c2c1a7cb1c
Sacha STAFYNIAK <sacha@digisquad.io>
2025-03-12 23:12:45 +0100

    chore: update ProseA and ProseOl components for improved prop typing and markup

--- .demo/app/components/prose/ProseA.vue
+++ .demo/app/components/prose/ProseA.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import type { PropType } from 'vue'
+
 defineProps({
   href: {
     type: String,
     default: '',
   },
   target: {
-    type: String,
+    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
     default: undefined,
     required: false,
   },
@@ -16,7 +18,7 @@ defineProps({
   <NuxtLink
     :href="href"
     :target="target"
-    class="px-1 text-muted-900 font-medium underline-offset-4 hover:underline dark:text-white"
+    class="text-muted-900 font-medium underline-offset-4 hover:underline dark:text-white"
   >
     <slot />
   </NuxtLink>
--- .demo/app/components/prose/ProseOl.vue
+++ .demo/app/components/prose/ProseOl.vue
@@ -1,5 +1,5 @@
 <template>
-  <BaseList ordered>
+  <ol class="list-decimal space-y-2 py-4 ps-4 ms-4 text-[0.95rem] text-muted-600 dark:text-muted-400">
     <slot />
-  </BaseList>
+  </ol>
 </template>
