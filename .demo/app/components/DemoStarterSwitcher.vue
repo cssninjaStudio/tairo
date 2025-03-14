e51d63ceef14b9245fe3e87d7a40911596f77a81
Driss Chelouati <driss.chelouati@gmail.com>
2025-03-14 21:19:54 +0100

    fix: adjust opacity for images in DemoStarterSwitcher and update navbar max-width

--- .demo/app/components/DemoStarterSwitcher.vue
+++ .demo/app/components/DemoStarterSwitcher.vue
@@ -42,18 +42,18 @@ const starters = [
   <div class="px-4 md:px-6 lg:px-8 py-20">
     <div class="max-w-3xl mx-auto">
       <div class="bg-muted-200/60 dark:bg-muted-800 rounded-xl p-4 grid grid-cols-2 md:grid-cols-3 gap-3">
-        <NuxtLink v-for="starter in starters" :key="starter.name" :to="starter.to" class="relative block bg-white dark:bg-muted-950 border border-muted-300 dark:border-muted-800 p-4 rounded-xl cursor-pointer" exact-active-class="border-primary-500! [&>svg]:block!">
+        <NuxtLink v-for="starter in starters" :key="starter.name" :to="starter.to" class="relative block bg-white dark:bg-muted-950 border border-muted-300 dark:border-muted-800 p-4 rounded-xl cursor-pointer" exact-active-class="border-primary-500! [&>svg]:block! [&>div>img]:opacity:100!">
           <Icon name="solar:check-circle-bold-duotone" class="absolute top-3 end-3 size-6 text-primary-500 dark:text-primary-400 hidden" />
           <div class="flex flex-col items-center justify-center">
             <img
               :src="starter.img"
               alt="Illustration"
-              class="w-28 mb-2 block dark:hidden"
+              class="w-28 mb-2 block dark:hidden opacity-60"
             >
             <img
               :src="starter.imgDark"
               alt="Illustration"
-              class="w-28 mb-2 hidden dark:block"
+              class="w-28 mb-2 hidden dark:block opacity-60"
             >
             <BaseHeading as="h4" size="sm" class="text-center text-muted-900 dark:text-muted-100">
               {{ starter.label }}
